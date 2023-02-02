2205c83e5ed39a4256ea2164e9af38613a65d379
Ilya Sokolov <sokolov.ilia.2000@mail.ru>
2023-02-02 19:28:50 +0300

    solution ex2

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+System.Console.WriteLine("Write a number: ");
+string number = Console.ReadLine();
+int length=number.Length;
+int index=0;
+if (length<3)
+{
+    System.Console.WriteLine("ERROR");
+}
+else
+{
+    System.Console.WriteLine(number[2]);
+}
