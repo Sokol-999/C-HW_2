27c0253624c4073a7cf80d7c23975284a20fd2ce
Ilya Sokolov <sokolov.ilia.2000@mail.ru>
2023-02-02 19:15:24 +0300

    solution ex1_2

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Write a three digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (99 < number && number < 1000)
+{
+    int num = number / 10 % 10;
+    System.Console.WriteLine(num);
+}
+else
+{
+    System.Console.WriteLine("It's a not three digit number");
+}
+
