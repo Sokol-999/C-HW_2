a53a62856780907a21dd156188155a98e90c03fe
Ilya Sokolov <sokolov.ilia.2000@mail.ru>
2023-02-02 19:14:55 +0300

    solution 1ex_1

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.WriteLine("Write a three digit number: ");
+string array = Console.ReadLine();
+
+int length = array.Length;
+
+if (length==3)
+{
+    System.Console.WriteLine("The second digit of the number: ");
+    System.Console.WriteLine(array[1]);
+}
+else
+{
+    System.Console.WriteLine("EROR-need a three digit number");
+}
+
